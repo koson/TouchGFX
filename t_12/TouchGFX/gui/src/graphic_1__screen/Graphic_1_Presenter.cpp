6cc3f5993d57e7fbb099aeeb5d3f56b1dd887a1e
Rivares <ura-nikolaevic@yandex.ru>
2021-09-29 01:03:05 +0300

    Забыл добавть конструктор в Презентер График 1

--- t_12/TouchGFX/gui/src/graphic_1__screen/Graphic_1_Presenter.cpp
+++ t_12/TouchGFX/gui/src/graphic_1__screen/Graphic_1_Presenter.cpp
@@ -1,6 +1,11 @@
 #include <gui/graphic_1__screen/Graphic_1_View.hpp>
 #include <gui/graphic_1__screen/Graphic_1_Presenter.hpp>
 
+
+Graphic_1_Presenter::Graphic_1_Presenter(Graphic_1_View& v) : view(v)
+{
+}
+
 void Graphic_1_Presenter::activate()
 {
     view.setData(model->getCurrentValue());
