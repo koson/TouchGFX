0a158d780552db395ad630d633535c26fb72528c
Rivares <ura-nikolaevic@yandex.ru>
2021-10-03 01:42:26 +0300

    Commented logger_SPI to Graphic_1_View

--- t_12/TouchGFX/gui/src/graphic_1__screen/Graphic_1_View.cpp
+++ t_12/TouchGFX/gui/src/graphic_1__screen/Graphic_1_View.cpp
@@ -51,8 +51,8 @@ void Graphic_1_View::setData(uint16_t data)
 {
   m_local_data_sensor = data;
 
-  Unicode::snprintf(loggerSPI_1Buffer, 200, "%d", static_cast<int>(m_local_data_sensor));
-  loggerSPI_1.invalidate();
+  // Unicode::snprintf(loggerSPI_1Buffer, 200, "%d", static_cast<int>(m_local_data_sensor));
+  // loggerSPI_1.invalidate();
 }
 
 
