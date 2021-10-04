69207649a88489bdfa8d596153c1cf5ed3c03b87
Rivares <ura-nikolaevic@yandex.ru>
2021-10-04 22:13:44 +0300

    Work with code_5

--- t_12/Core/Inc/global_pins.h
+++ t_12/Core/Inc/global_pins.h
@@ -2,9 +2,9 @@
 #define GLOBAL_PINS_H
 
 // Размерности портов
-uint8_t countAI = 4;
-uint8_t countAO = 4;
-uint8_t countDI = 4;
-uint8_t countDO = 4;
+const uint8_t countAI = 4;
+const uint8_t countAO = 4;
+const uint8_t countDI = 4;
+const uint8_t countDO = 4;
 
 #endif // GLOBAL_PINS_H
